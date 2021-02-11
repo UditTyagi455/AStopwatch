7181dbb9873417311a993dd83a31a2c75f0a234d
UditTyagi455 <57524054+UditTyagi455@users.noreply.github.com>
2021-02-11 08:31:40 -0500

    Update stopwatch.js

--- project1/stopwatch.js
+++ project1/stopwatch.js
@@ -53,7 +53,7 @@ document.getElementById("rt").disabled = true;
                      count=0;
                  }  
                 
-             },1);
+             },10);
             s.innerHTML ="Stop";
             document.getElementById("r").disabled = true;
             document.getElementById("rt").disabled = false;
